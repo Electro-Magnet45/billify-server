c02e44c00727b65ac26a463ef31af966684ddbf2
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-03-02 14:27:17 +0530

    'billify-server'

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const io = socketIO(server, {
 io.on("connection", (socket) => {
   console.log("Client connected");
   socket.on("disconnect", () => console.log("Client disconnected"));
+  socket.on("finishPayment", (mgs) => console.log(mgs));
 });
 
 setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
