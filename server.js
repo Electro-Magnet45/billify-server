aa863775c428bbe742df81c941707b1b8820134c
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-03-03 10:43:38 +0530

    Eight Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const INDEX = "/index.html";
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT);
 
 const io = socketIO(server, {
   cors: {
