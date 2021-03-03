00e0a46ea8651c9aa277303b8e80faab6cbf2223
TheAswin <75930721+TheAswin@users.noreply.github.com>
2021-03-03 10:15:24 +0530

    Sixth Commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const socketIO = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const INDEX = "/index.html";
 
 const server = express()
@@ -17,11 +17,8 @@ const io = socketIO(server, {
 
 io.on("connection", (socket) => {
   console.log("Client connected");
-  socket.on("disconnect", () => console.log("Client disconnected"));
 
-  socket.on("joinert", (message) => {
-    io.emit("receive", message);
+  socket.on("hello", (msg) => {
+    console.log(msg);
   });
 });
-
-setInterval(() => io.emit("time", new Date().toTimeString()), 1000);
